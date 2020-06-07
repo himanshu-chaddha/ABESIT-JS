ab95b79ffef7ea5e608e75a39cf4d5421fe166cf
himanshu-chaddha <himanshu.chaddha@mail.vinove.com>
2020-06-07 15:54:17 +0530

    git basics

---
+++ jQuery/exercise1/exercise.js
@@ -0,0 +1,13 @@
+$('div').css("background-color","purple");
+
+$('.highlight').css("fontSize","100px");
+
+$('#third').css("border","10px solid orange");
+
+// $('div:nth-of-type(1)').css("color", "red");
+
+// $('div:first-of-type').css("color", "pink");
+// $($('div')[0]).css("color", "pink");
+
+
+$('div').first().css("color", "pink");
