19ca6283387fa4471cdc79844d57ba5caea571b7
himanshu-chaddha <himanshu.chaddha@mail.vinove.com>
2020-06-10 21:54:17 +0530

    jQuery events completed

--- jQuery/exercise1/exercise.js
+++ jQuery/exercise1/exercise.js
@@ -1,10 +1,10 @@
-$('div').css("background-color","purple");
+$('div').css("backgroundColor", "purple");
 
-$('.highlight').css("fontSize","100px");
+$('.highlight').css('fontSize', '100px');
 
-$('#third').css("border","10px solid orange");
+$('#third').css('border', "5px solid orange");
 
-// $('div:nth-of-type(1)').css("color", "red");
+// $('div:nth-of-type(1)').css('color', 'pink')
 
 // $('div:first-of-type').css("color", "pink");
 // $($('div')[0]).css("color", "pink");
